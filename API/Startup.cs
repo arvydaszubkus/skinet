30661eb3cf1d617f8863fae550569d7112a383e0
Arvydas <zubkus.arvydas@gmail.com>
2022-03-13 10:25:10 +0200

    End of section6

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,14 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
             
         }
 
@@ -55,6 +63,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
